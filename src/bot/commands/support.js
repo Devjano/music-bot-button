199ae2583226e546133989760a28168cc35bfb14
JANO <77622056+Devjano@users.noreply.github.com>
2021-09-16 17:47:22 -0700

    Create support.js

---
+++ src/bot/commands/support.js
@@ -0,0 +1,30 @@
+module.exports = {
+    name: "support",
+    cooldown: 3,
+    description: 'Bot Orders',
+    run: async function(client, message, args, user) {
+        try {
+            const { MessageEmbed } = require('discord.js');
+            var prefix = await require('quick.db').fetch(`prefix_${message.guild.id}`);
+            if (prefix == null) prefix = require('../../config/bot').prefix;
+            let help = new MessageEmbed()
+                .setColor('BLUE')
+               
+               
+                .setDescription(`
+ **[ thanks to join server ](https://discord.gg/2ugMEJubkt)**   
+   `);
+
+
+              
+         
+            setTimeout(() => {
+                message.channel.send({
+                    embed: help
+                })
+            }, 500);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+};
