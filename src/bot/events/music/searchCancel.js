98e85eba4df55b74aa1c0dc9b5ffad8cd7b418d5
JANO <77622056+Devjano@users.noreply.github.com>
2021-09-16 17:38:38 -0700

    Delete src/bot/events/music directory

--- src/bot/events/music/searchCancel.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function(client, message) {
-    var { MessageEmbed } = require('discord.js');
-    var msg = require('quick.db').fetch(`Delete_${message.channel.id}`);
-    var deleted = new MessageEmbed()
-        .setTitle('<a:jano_26:799630865474256972> | **Timeout**')
-        .setColor('BLUE')
-    message.channel.messages.fetch(msg).then(m => m.edit({ embed: deleted })).catch(err => { return });
-};
