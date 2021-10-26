dd9f896c0398595e86eb13e9dfa6d5abd836926e
JANO <77622056+Devjano@users.noreply.github.com>
2021-10-25 18:02:32 -0700

    Update addSong.js

--- src/bot/events/music/addSong.js
+++ src/bot/events/music/addSong.js
@@ -3,9 +3,9 @@ module.exports = async function(client, message, queue, song) {
     message.channel.send(
         new MessageEmbed()
         .setAuthor(song.name, song.thumbnail, song.url)
-        .setColor('BLUE')
+        .setColor('RANDOM')
         .setThumbnail(song.thumbnail)
-        .setDescription(`<a:jano_50:872719123770114058> | **__[${song.name}](${song.url})__** **has been add to queue**`)
+        .setDescription(`:white_check_mark:  | **__[${song.name}](${song.url})__** **has been add to queue**`)
         .setFooter(song.formattedDuration + ` | ${song.likes}👍 ${song.dislikes}👎`)
     );
 };
