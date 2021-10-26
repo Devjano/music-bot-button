8531762a1cfd03ca801026a11751a8958697e5be
JANO <77622056+Devjano@users.noreply.github.com>
2021-10-25 17:17:27 -0700

    Update ping.js

--- src/bot/commands/ping.js
+++ src/bot/commands/ping.js
@@ -4,14 +4,14 @@ module.exports = {
     description: 'view the bot ping',
     aliases: ["ping"],
     run: async function(client, message, args, user) {
-        var states = "<a:jano_13:804354511355117598> Excellent";
-        var states2 = "<a:jano_13:804354511355117598> Excellent";
+        var states = "";
+        var states2 = "";
         var msg = `${Date.now() - message.createdTimestamp}`;
         var api = `${Math.round(client.ws.ping)}`;
         if (message.author.bot) return;
         message.channel.send({
             embed: {
-                description: `**Time Taken:** ${msg} ms <a:jano_43:840250638483062784> | ${states}\n**WebSocket:** ${api} ms <a:jano_43:840250638483062784> | ${states2}`,
+                description: `**Time Taken:** ${msg} ms :signal_strength: | ${states}\n**WebSocket:** ${api} ms :signal_strength: | ${states2}`,
                 color: 0x2F3136
             }
         });
