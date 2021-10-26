f8e43526291fc53719a666edbd5cb43c6fa3261a
JANO <77622056+Devjano@users.noreply.github.com>
2021-10-25 18:06:04 -0700

    Update searchCancel.js

--- src/bot/events/music/searchCancel.js
+++ src/bot/events/music/searchCancel.js
@@ -2,7 +2,7 @@ module.exports = async function(client, message) {
     var { MessageEmbed } = require('discord.js');
     var msg = require('quick.db').fetch(`Delete_${message.channel.id}`);
     var deleted = new MessageEmbed()
-        .setTitle('<a:jano_26:799630865474256972> | **Timeout**')
-        .setColor('BLUE')
+        .setTitle(':x:  | **Timeout**')
+        .setColor('RANDOM')
     message.channel.messages.fetch(msg).then(m => m.edit({ embed: deleted })).catch(err => { return });
 };
